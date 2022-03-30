cece9fbabd3f7ba82f12af1ce88c1e55bc8ff1dc
kazuki <kaz.tokyo.tech20@gmail.com>
2022-03-30 17:41:31 +0900

    fix type annotation

--- senkyo_match/libs/utils/os/read_json.py
+++ senkyo_match/libs/utils/os/read_json.py
@@ -1,6 +1,6 @@
 import json
 
 
-def read_json(json_file_path: str) -> dict:
+def read_json(json_file_path: str) -> list[dict]:
     with open(json_file_path, "r") as f:
         return json.load(f)
